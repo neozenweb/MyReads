52558947a9aa94c140a4695441bf56bf5945c38b
neozenweb <37545009+neozenweb@users.noreply.github.com>
2018-12-05 20:46:16 -0500

    Updates

---
+++ precache-manifest.bb21c0b64349682497c88953adac2ff3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "970c0060ff174c587b9c",
+    "url": "/MyReads/static/js/runtime~main.970c0060.js"
+  },
+  {
+    "revision": "9329584fc4bab40fc35a",
+    "url": "/MyReads/static/js/main.9329584f.chunk.js"
+  },
+  {
+    "revision": "94d7c0e35b8b4ed2af6b",
+    "url": "/MyReads/static/js/1.94d7c0e3.chunk.js"
+  },
+  {
+    "revision": "9329584fc4bab40fc35a",
+    "url": "/MyReads/static/css/main.6e3b461b.chunk.css"
+  },
+  {
+    "revision": "dff3e345d7f74ffd70993f4a0fcfe25f",
+    "url": "/MyReads/index.html"
+  }
+];
